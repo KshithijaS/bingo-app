0ee7e91a401dd7c2deedd5940078fd2b3e5e3f92
KshithijaS <kshithijas2003@gmail.com>
2020-08-08 17:49:50 +0530

    included js folder

---
+++ Front-end-pages/js/landing-main.js
@@ -0,0 +1,6 @@
+let dots = document.querySelectorAll('.dot');
+let page_tracker = document.getElementById('page_tracker')
+
+if (page_tracker.style.background == '#FFC37B') {
+    dots.style.background = '#000';
+}
