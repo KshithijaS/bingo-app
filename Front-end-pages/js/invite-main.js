09d9dd932e065ec1a842ee027cc82f4307ec6618
KshithijaS <kshithijas2003@gmail.com>
2020-08-10 15:21:53 +0530

    Added invite page
    html, css, js files for the invite page

---
+++ Front-end-pages/js/invite-main.js
@@ -0,0 +1,9 @@
+
+function createID() {
+    document.getElementById("dropdown1").style.display = "block";
+}
+
+function enterID() {
+    document.getElementById("dropdown2").style.display = "block";
+}
+
