b7d82855af8802b051e25a07d364e4de3c0a310e
KshithijaS <69100816+KshithijaS@users.noreply.github.com>
2020-08-22 12:06:33 +0530

    Added password reset

--- Fringo/main/urls.py
+++ Fringo/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.landing, name="landing"),
@@ -11,4 +12,10 @@ urlpatterns = [
     path('invite/', views.invite, name="invite"),
     path('myprofile/', views.profile, name="profile"),
     path('game/', views.game, name="game"),
+    
+    path('resetPassword/', auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"), name="reset_password"),
+    path('resetPassword_sent/', auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_form.html"), name="password_reset_confirm"),
+    path('resetPassword_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_done.html"), name="password_reset_complete"),
+
 ]
